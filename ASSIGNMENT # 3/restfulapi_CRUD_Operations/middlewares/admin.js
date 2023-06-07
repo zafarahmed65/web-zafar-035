caff22785c0725e366177e75709ca2b931ef40cb
zafarahmed65 <118745580+zafarahmed65@users.noreply.github.com>
2023-06-07 23:46:51 +0500

    Delete ASSIGNMENT # 3/restfulapi_CRUD_Operations directory

--- ASSIGNMENT # 3/restfulapi_CRUD_Operations/middlewares/admin.js
+++
@@ -1,6 +0,0 @@
-function admin(req, res, next) {
-    if(req.user.role!="admin")
-        return res.status(403).send("Your are not authorized");
-    next();
-}
-module.exports = admin;
--- ASSIGNMENT # 3/restfulapi_CRUD_Operations/middlewares/validateService.js
+++
@@ -1,8 +0,0 @@
-const { validate } = require("../models/service");
-function validateService(req, res, next) {
-  let { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-  next();
-}
-
-module.exports = validateService;
